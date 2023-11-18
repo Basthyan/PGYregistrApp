3fd6d55bf47bf5e4591260ee8a2436f1f5629f47
AgustinQuezada <agustin.tomas.quezada.morales@gmail.com>
2023-11-18 00:23:33 -0300

    detalles

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router,NavigationExtras } from '@angular/router';
-import { AlertController, Animation, AnimationController} from '@ionic/angular';
+import { Router, NavigationExtras } from '@angular/router';
+import {
+  AlertController,
+  Animation,
+  AnimationController,
+} from '@ionic/angular';
 import type { QueryList } from '@angular/core';
 import { Usuario } from '../modelo/usuario';
 import { Perfil } from '../modelo/perfil';
@@ -14,69 +18,76 @@ import { ApiService } from 'src/services/api.service';
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-
 export class LoginPage implements OnInit {
-  
+  constructor(
+    private router: Router,
+    private animationCtrl: AnimationController,
+    private auth: AuthGuard,
+    private api: ApiService,
+    private alertController: AlertController
+  ) {}
 
-  constructor(private router: Router,private animationCtrl: AnimationController, private auth:AuthGuard, private api:ApiService, private alertController :AlertController) { }
-
-  private animation!: Animation;
   private typeuser!: Usuario;
-  private typePerfil!: Perfil;
-  private curso!:Curso;
 
-  textBtn = "INGRESAR";
-  textUser = "Usuario";
-  textPass = "Contraseña";
-  desUser = "ingrese usuario";
-  desPass = "ingrese contraseña";
-
-
+  textBtn = 'INGRESAR';
+  textUser = 'Usuario';
+  textPass = 'Contraseña';
+  desUser = 'ingrese usuario';
+  desPass = 'ingrese contraseña';
 
   // user={
   //   apellido:"Ejemplo ngmodel"
   // }
 
-    usuario = new FormGroup({
-    username: new FormControl('',[Validators.required, Validators.minLength(4),Validators.maxLength(20)]),
-    password: new FormControl('',[Validators.required, Validators.minLength(4),Validators.maxLength(20)]),
+  usuario = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(20),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(20),
+    ]),
   });
 
-  apiLogin() {
-    this.api.login(this.usuario.value.username!, this.usuario.value.password!).subscribe(
-      (response) => {
-        this.typeuser = response.body as unknown as Usuario;
-        console.log("estado:" + response.status);
-        if (response.status == 200) {
-          let setData: NavigationExtras = {
-            state: {
-              id: this.typeuser.id,
-              user: this.typeuser.user,
-              correo: this.typeuser.correo,
-              nombre: this.typeuser.nombre,
-              tipoPerfil: this.typeuser.tipoPerfil
+  Login() {
+    this.api
+      .login(this.usuario.value.username!, this.usuario.value.password!)
+      .subscribe(
+        (response) => {
+          this.typeuser = response.body as unknown as Usuario;
+          console.log('estado:' + response.status);
+          if (response.status == 200) {
+            let setData: NavigationExtras = {
+              state: {
+                id: this.typeuser.id,
+                user: this.typeuser.user,
+                correo: this.typeuser.correo,
+                nombre: this.typeuser.nombre,
+                tipoPerfil: this.typeuser.tipoPerfil,
+              },
+            };
+
+            console.log('estado:' + this.typeuser.tipoPerfil);
+
+            if (this.typeuser.tipoPerfil === 1) {
+              this.auth.setAuthenticationStatus(true);
+              this.router.navigate(['/home-profesor'], setData);
             }
-          };
 
-          console.log("estado:"+this.typeuser.tipoPerfil);
-
-          if (this.typeuser.tipoPerfil === 1) {
-            this.auth.setAuthenticationStatus(true);
-            this.router.navigate(['/home-profesor'], setData);
+            if (this.typeuser.tipoPerfil === 2) {
+              this.auth.setAuthenticationStatus(true);
+              this.router.navigate(['/home'], setData);
+            }
           }
-
-          if (this.typeuser.tipoPerfil === 2) {
-            this.auth.setAuthenticationStatus(true);
-            this.router.navigate(['/home'], setData);
-          }
+        },
+        (error) => {
+          console.error('Error en inicio de sesión:', error);
         }
-      },
-      (error) => {
-        console.error('Error en inicio de sesión:', error);
-      });
-  }
-  
-  ngOnInit() {
+      );
   }
 
+  ngOnInit() {}
 }
