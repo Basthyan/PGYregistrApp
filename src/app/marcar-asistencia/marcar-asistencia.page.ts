f8090c3f8fb7a1957c41d9dc2d72e13da626358e
AgustinQuezada <agustin.tomas.quezada.morales@gmail.com>
2023-11-20 20:00:56 -0300

    estetica

--- src/app/marcar-asistencia/marcar-asistencia.page.ts
+++ src/app/marcar-asistencia/marcar-asistencia.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-marcar-asistencia',
@@ -7,7 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MarcarAsistenciaPage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) { }
+
+  abrirCorreo() {
+    // Dirección de correo electrónico y otros parámetros opcionales
+    let destinatario = 'profesor@duocuc.cl';
+    let asunto = 'Marcaje de asistencia';
+    let cuerpo = 'Cuerpo del correo';
+
+    // Construir el enlace de correo electrónico con la sintaxis "mailto"
+    let mailtoLink = `mailto:${destinatario}?subject=${encodeURIComponent(asunto)}&body=${encodeURIComponent(cuerpo)}`;
+
+    // Abrir la aplicación de correo electrónico predeterminada del usuario
+    window.location.href = mailtoLink;
+  }
+
+  volver() {
+    this.navCtrl.navigateForward('/home');
+  }
 
   ngOnInit() {
   }
